b38df0e1672da7edf940fd73fc46c19b3901edaf
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 17:28:43 -0500

    confirm rtk query works through showlist

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -1,14 +1,15 @@
-import { useGetShowsQuery } from "../services/api";
+import { useGetShowsQuery } from "../features/shows/show-slice";
 
 const Shows = () => {
   const { data, error, isLoading, isSuccess, isError } = useGetShowsQuery();
-  debugger;
   return (
     <div>
       This is the shows page
       {isLoading && "Loading..."}
       {isError && error.message}
-      {isSuccess && data && data.map((show) => <h1 key={show.id}>{show}</h1>)}
+      {isSuccess &&
+        data &&
+        data.map((show) => <h2 key={show.id}>{show.venue}</h2>)}
     </div>
   );
 };
