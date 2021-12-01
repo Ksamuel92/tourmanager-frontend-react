f50ed5db5e5ea5cd75fb64acc453505295b9d5bc
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 16:18:29 -0500

    build out Shows.js

--- src/pages/Shows.js
+++ src/pages/Shows.js
@@ -1,3 +1,16 @@
-const Show = () => {
-  return <div>This is the shows page</div>;
+import { useGetShowsQuery } from "../services/api";
+
+const Shows = () => {
+  const { data, error, isLoading, isSuccess, isError } = useGetShowsQuery();
+  debugger;
+  return (
+    <div>
+      This is the shows page
+      {isLoading && "Loading..."}
+      {isError && error.message}
+      {isSuccess && data && data.map((show) => <h1 key={show.id}>{show}</h1>)}
+    </div>
+  );
 };
+
+export default Shows;
