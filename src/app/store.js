7ad68899891ed09db5270ddd3b3cddb121dd3433
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 14:39:34 -0500

    change index.js back to store.js

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { tourManagerApi } from "../services/api";
+
+export const store = configureStore({
+  reducer: {
+    [tourManagerApi.reducerPath]: tourManagerApi.reducer,
+    show: showReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(tourManagerApi.middleware),
+});
+
+setupListeners(store.dispatch());
