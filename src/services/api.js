3e06d60d51cb9c93c38be57298b9d0a9bef7f9cb
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 17:16:28 -0500

    add "LIST" to tagTypes in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,23 +1,21 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { getState } from "@reduxjs/toolkit/";
+// import { getState } from "@reduxjs/toolkit/";
+
 export const tourManagerApi = createApi({
   reducerPath: "tourManagerApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3001/",
-    prepareHeaders: (headers, { getState }) => {
-      const token = getState().auth.token;
-      if (token) {
-        headers.set("authorization", `Bearer ${token}`);
-      }
+    // prepareHeaders: (headers, { getState }) => {
+    //   const token = getState().auth.token;
+    //   if (token) {
+    //     headers.set("authorization", `Bearer ${token}`);
+    //   }
 
-      return headers;
-    },
-    endpoints: (builder) => ({
-      getShows: builder.query({
-        query: () => "shows",
-      }),
-    }),
+    //   return headers;
+    // },
   }),
+  tagTypes: ["Show", "User", "Promoter", "UNAUTHORIZED", "LIST"],
+  endpoints: () => ({}),
 });
 
 export const { useGetShowsQuery } = tourManagerApi;
