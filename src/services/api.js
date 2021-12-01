4cb5b9f8c750327e9518fa732f17baeb4c67b1a0
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 17:29:02 -0500

    clean up comments in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -5,17 +5,7 @@ export const tourManagerApi = createApi({
   reducerPath: "tourManagerApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://localhost:3001/",
-    // prepareHeaders: (headers, { getState }) => {
-    //   const token = getState().auth.token;
-    //   if (token) {
-    //     headers.set("authorization", `Bearer ${token}`);
-    //   }
-
-    //   return headers;
-    // },
   }),
   tagTypes: ["Show", "User", "Promoter", "UNAUTHORIZED", "LIST"],
   endpoints: () => ({}),
 });
-
-export const { useGetShowsQuery } = tourManagerApi;
