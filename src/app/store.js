09cc569b2235e44a44b23daeca462579404cad73
Kyle Samuel <ksamuel92@gmail.com>
2021-12-01 17:16:00 -0500

    remove showReducer from store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { tourManagerApi } from "../services/api";
-import { showReducer } from "../store/shows/show-slice";
 
 export const store = configureStore({
   reducer: {
     [tourManagerApi.reducerPath]: tourManagerApi.reducer,
-    show: showReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tourManagerApi.middleware),
