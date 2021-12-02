ee225eb996e89b3c96b07d2326ad295b6d74bd9b
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 14:01:01 -0500

    add signupform to app.js for test purposes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import NewShowForm from "./components/shows/NewShowForm";
 import Promoters from "./pages/Promoters";
 import PromoterDetails from "./components/promoters/PromoterDetails";
 import NewPromoterForm from "./components/promoters/NewPromoterForm";
+import SignUpForm from "./components/users/SignUpForm";
 import { Routes, Route } from "react-router-dom";
 
 import LandingGrid from "./layout/LandingGrid";
@@ -17,6 +18,7 @@ function App() {
     <div className="App">
       <Layout>
         <LandingHeader />
+        <SignUpForm />
         <main>
           <Routes>
             <Route path="/" element={<LandingHeader />}>
