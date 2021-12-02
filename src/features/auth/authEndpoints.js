45ff1437c2c38ee17771d835eb09bfdb9d20e26b
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 10:43:32 -0500

    export authApi actions

--- src/features/auth/authEndpoints.js
+++ src/features/auth/authEndpoints.js
@@ -1,26 +1,29 @@
 import { tourManagerApi } from "../../services/api";
 
-const authApiEndpoints = tourManagerApi.injectEndpoints({
+export const authApiEndpoints = tourManagerApi.injectEndpoints({
   endpoints: (builder) => ({
     signup: builder.mutation({
       query: (credentials) => ({
-        url: 'signup',
-        method: 'POST',
-        body: credentials
-      })
+        url: "signup",
+        method: "POST",
+        body: credentials,
+      }),
     }),
     login: builder.mutation({
       query: (credentials) => ({
-        url: 'login',
-        method: 'POST',
-        body: credentials
-      })
+        url: "login",
+        method: "POST",
+        body: credentials,
+      }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: 'logout',
-        method: 'DELETE',
-      })
-    })
-  }
-})
+        url: "logout",
+        method: "DELETE",
+      }),
+    }),
+  }),
+});
+
+export const { useLoginQuery, useSignupQuery, useLogoutQuery } =
+  authApiEndpoints;
