68ecb67dc407a76cf82552536f307ca288e68090
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 11:53:11 -0500

    add LandingHeader.js to refactor App.js

---
+++ src/layout/LandingHeader.js
@@ -0,0 +1,16 @@
+import { Container } from "@material-ui/core";
+import { Typography } from "@mui/material";
+
+const LandingHeader = () => {
+  return (
+    <Container>
+      <Typography variant="h1">TourManager</Typography>
+      <Typography variant="subtitle1">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+        blanditiis tenetur
+      </Typography>
+    </Container>
+  );
+};
+
+export default LandingHeader;
