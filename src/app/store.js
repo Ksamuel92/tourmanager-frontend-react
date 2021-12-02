73f8aea5560f0f8e87b072294bccb588bf18a56e
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 15:09:58 -0500

    add AuthReducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { tourManagerApi } from "../services/api";
+import authSlice from "../features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     [tourManagerApi.reducerPath]: tourManagerApi.reducer,
+    authReducer: authSlice,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(tourManagerApi.middleware),
