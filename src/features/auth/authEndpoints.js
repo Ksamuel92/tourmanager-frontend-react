ea4cde2f37140693a262981acd9720fb39ff6bb4
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 15:09:44 -0500

    add Lifecyle Method to authApi signUp endpoint

--- src/features/auth/authEndpoints.js
+++ src/features/auth/authEndpoints.js
@@ -1,29 +1,42 @@
 import { tourManagerApi } from "../../services/api";
+import { setCredentials } from "../../features/auth/authSlice";
 
 export const authApiEndpoints = tourManagerApi.injectEndpoints({
   endpoints: (builder) => ({
-    signup: builder.mutation({
+    signUp: builder.mutation({
       query: (credentials) => ({
         url: "signup",
         method: "POST",
         body: credentials,
       }),
-    }),
-    login: builder.mutation({
-      query: (credentials) => ({
-        url: "login",
-        method: "POST",
-        body: credentials,
+      async onQueryStarted(credentials, { dispatch, queryFulfilled }) {
+        try {
+          const { data, meta } = await queryFulfilled;
+          //onSuccess side-effect
+          const headers = meta.response.headers;
+          const token = headers.get("Authorization");
+          dispatch(setCredentials({ data, token }));
+        } catch (err) {
+          //onError side-effect
+          // TODO: Handle error message here
+        }
+      },
+      login: builder.mutation({
+        query: (credentials) => ({
+          url: "login",
+          method: "POST",
+          body: credentials,
+        }),
       }),
-    }),
-    logout: builder.mutation({
-      query: () => ({
-        url: "logout",
-        method: "DELETE",
+      logout: builder.mutation({
+        query: () => ({
+          url: "logout",
+          method: "DELETE",
+        }),
       }),
     }),
   }),
 });
 
-export const { useLoginQuery, useSignupQuery, useLogoutQuery } =
+export const { useLoginMutation, useSignUpMutation, useLogoutMutation } =
   authApiEndpoints;
