91cad37594543e404b7d657006632e5fdc835dc9
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 16:47:28 -0500

    create LandingPage.js

---
+++ src/pages/LandingPage.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import LandingHeader from "../layout/LandingHeader";
+import LandingGrid from "../layout/LandingGrid";
+
+const LandingPage = () => {
+  return (
+    <Fragment>
+      <LandingHeader />
+      <LandingGrid />
+    </Fragment>
+  );
+};
+export default LandingPage;
