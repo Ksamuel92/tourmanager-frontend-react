7ae330ba34b9e0e8a12dd6cef562006fbb892695
Kyle Samuel <ksamuel92@gmail.com>
2021-12-02 11:54:57 -0500

    add Routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "./App.css";
+import Shows from "./pages/Shows";
+import ShowDetails from "./components/shows/ShowDetails";
+import NewShowForm from "./components/shows/NewShowForm";
+import Promoters from "./pages/Promoters";
+import PromoterDetails from "./components/promoters/PromoterDetails";
+import NewPromoterForm from "./components/promoters/NewPromoterForm";
+import { Routes, Route } from "react-router-dom";
 
 import LandingGrid from "./layout/LandingGrid";
 import LandingHeader from "./layout/LandingHeader";
 
 import Layout from "./layout/Layout";
-import Shows from "./pages/Shows";
-import Promoters from "./pages/Promoters";
 function App() {
   return (
     <div className="App">
       <Layout>
         <LandingHeader />
-        <LandingGrid />
-        <Shows />
-        <Promoters />
+        <main>
+          <Routes>
+            <Route path="/" element={<LandingHeader />}>
+              <Route path="shows" element={<Shows />}>
+                <Route path=":id" element={<ShowDetails />} />
+                <Route path="new" element={<NewShowForm />} />
+              </Route>
+              <Route path="promoters" element={<Promoters />}>
+                <Route path=":slug" element={<PromoterDetails />} />
+                <Route path="new" element={<NewPromoterForm />} />
+              </Route>
+            </Route>
+          </Routes>
+          <Shows />
+          <LandingGrid />
+        </main>
       </Layout>
     </div>
   );
