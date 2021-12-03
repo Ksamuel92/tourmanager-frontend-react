86e1fa7b41eb9fc06a9c8cf23f93e965f913d88f
Kyle Samuel <ksamuel92@gmail.com>
2021-12-03 13:37:37 -0500

    add DatePicker with onChange method in NewShowForm

--- src/components/shows/NewShowForm.js
+++ src/components/shows/NewShowForm.js
@@ -1,10 +1,29 @@
 import { TextField } from "@mui/material";
+import DatePicker from "@mui/lab/DatePicker";
+import { useState } from "react";
 
 const NewShowForm = () => {
+  const [showFormState, setShowFormState] = useState({});
+
+  const handleChange = (e) => {
+    const [name, value] = e.target;
+    setShowFormState({
+      ...showFormState,
+      [name]: value,
+    });
+  };
+
   return (
     <form>
       <div>
         <TextField id="venue" name="venue" label="Venue" required />
+        <DatePicker
+          name="date"
+          label="Date"
+          value={showFormState.date}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
       </div>
     </form>
   );
