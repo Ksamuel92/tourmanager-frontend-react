f31b86c1493602fb5598fa69b9d21da07919ef9d
Kyle Samuel <ksamuel92@gmail.com>
2021-12-08 12:02:09 -0500

    add ElevationScroll to NavBar.js

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -7,6 +7,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
+import { useScrollTrigger } from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,29 +28,44 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+function ElevationScroll(props) {
+  const { children } = props;
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  });
+
+  return React.cloneElement(children, {
+    elevation: trigger ? 4 : 0,
+  });
+}
+
 const NavBar = () => {
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            TourManager
-          </Typography>
-          <Link to={"auth"} className={classes.login}>
-            <Button color="inherit">Login</Button>
-          </Link>
-        </Toolbar>
-      </AppBar>
+      <ElevationScroll>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              TourManager
+            </Typography>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Link to={"auth"} className={classes.login}>
+              <Button color="inherit">Login</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
     </div>
   );
 };
