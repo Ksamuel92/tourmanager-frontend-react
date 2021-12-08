79037be97b5f909b7dfd10cb5d3cb87678b4952b
Kyle Samuel <ksamuel92@gmail.com>
2021-12-08 14:43:43 -0500

    improve NavBar styling, implement react-router

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,24 +1,23 @@
-import React from "react";
+import { useState, useEffect, cloneElement } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import { Link } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { useScrollTrigger } from "@mui/material";
+import { Tabs, Tab } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    marginBottom: 5,
-  },
   menuButton: {
     marginRight: theme.spacing(2),
   },
   title: {
     flexGrow: 1,
+  },
+  tab: {
+    minWidth: 10,
+    marginLeft: "25px",
   },
   login: {
     margin: "1rem",
@@ -35,31 +34,71 @@ function ElevationScroll(props) {
     threshold: 0,
   });
 
-  return React.cloneElement(children, {
+  return cloneElement(children, {
     elevation: trigger ? 4 : 0,
   });
 }
 
 const NavBar = () => {
+  const [value, setValue] = useState(0);
   const classes = useStyles();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.pathname === "/" && value !== 0) {
+      setValue(0);
+    } else if (location.pathname === "/shows" && value !== 1) {
+      setValue(1);
+    } else if (location.pathname === "/promoters" && value !== 2) {
+      setValue(2);
+    }
+  }, [value, location]);
+
+  const handleChange = (e, value) => {
+    setValue(value);
+  };
 
   return (
-    <div className={classes.root}>
+    <div sx={{ flexGrow: 1 }}>
       <ElevationScroll>
         <AppBar position="fixed" color="primary">
           <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              TourManager
-            </Typography>
-            <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
+            <Button>
+              <Typography variant="h6" className={classes.title}>
+                TourManager
+              </Typography>
+            </Button>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              sx={{ marginLeft: "auto" }}
             >
-              <MenuIcon />
-            </IconButton>
-
+              <Tab
+                className={classes.tab}
+                label="Home"
+                component={NavLink}
+                to="/"
+              />
+              <Tab
+                className={classes.tab}
+                label="Shows"
+                component={NavLink}
+                to="/shows"
+              />
+              <Tab
+                className={classes.tab}
+                label="Promoters"
+                component={NavLink}
+                to="/promoters"
+              />
+              {/* <Tab className={classes.tab} label="Schedule" component={Link}/> */}
+              {/* <Tab
+                className={classes.tab}
+                label="User Profile"
+                component={Link}
+              />
+              Calendar Page */}
+            </Tabs>
             <Link to={"auth"} className={classes.login}>
               <Button color="inherit">Login</Button>
             </Link>
