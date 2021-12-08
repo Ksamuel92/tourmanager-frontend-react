9289438dc75c152cf83aa65e9c322120df9b452b
Kyle Samuel <ksamuel92@gmail.com>
2021-12-08 17:40:11 -0500

    build out login endpoint, start current user

--- src/features/auth/authEndpoints.js
+++ src/features/auth/authEndpoints.js
@@ -1,5 +1,6 @@
 import { tourManagerApi } from "../../services/api";
 import { setCredentials } from "../../features/auth/authSlice";
+import { useNavigate } from "react-router-dom";
 
 export const authApiEndpoints = tourManagerApi.injectEndpoints({
   endpoints: (builder) => ({
@@ -10,33 +11,52 @@ export const authApiEndpoints = tourManagerApi.injectEndpoints({
         body: credentials,
       }),
       async onQueryStarted(credentials, { dispatch, queryFulfilled }) {
+        const navigate = useNavigate;
         try {
           const { data, meta } = await queryFulfilled;
           //onSuccess side-effect
           const headers = meta.response.headers;
           const token = headers.get("Authorization");
           dispatch(setCredentials({ data, token }));
+          navigate("/shows");
         } catch (err) {
           //onError side-effect
           // TODO: Handle error message here
         }
       },
-      login: builder.mutation({
-        query: (credentials) => ({
-          url: "login",
-          method: "POST",
-          body: credentials,
-        }),
+    }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "login",
+        method: "POST",
+        body: credentials,
       }),
-      logout: builder.mutation({
-        query: () => ({
-          url: "logout",
-          method: "DELETE",
-        }),
+      async onQueryStarted(credentials, { dispatch, queryFulfilled }) {
+        try {
+          const { data, meta } = await queryFulfilled;
+          const headers = meta.response.headers;
+          const token = headers.get("Authorization");
+          dispatch(setCredentials({ data, token }));
+        } catch (err) {
+          //TODO: Handle Error message
+        }
+      },
+    }),
+    logout: builder.mutation({
+      query: () => ({
+        url: "logout",
+        method: "DELETE",
       }),
+    }),
+    currentUser: builder.query({
+      query: () => "current_user",
     }),
   }),
 });
 
-export const { useLoginMutation, useSignUpMutation, useLogoutMutation } =
-  authApiEndpoints;
+export const {
+  useLoginMutation,
+  useSignUpMutation,
+  useLogoutMutation,
+  useCurrentUserQuery,
+} = authApiEndpoints;
