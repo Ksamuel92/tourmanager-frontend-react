ecdd19ac0ddbe15391bee24c2ca22afb8a4557be
Kyle Samuel <ksamuel92@gmail.com>
2021-12-09 13:33:35 -0500

    refactor Promoters page

--- src/pages/Promoters.js
+++ src/pages/Promoters.js
@@ -1,15 +1,10 @@
-import { useGetPromotersQuery } from "../features/promoters/promoter-slice";
+import PromoterList from "../components/promoters/PromoterList";
 
 const Promoters = () => {
-  const { data, error, isLoading, isSuccess, isError } = useGetPromotersQuery();
   return (
     <div>
       This is the Promoters page
-      {isLoading && "Loading..."}
-      {isError && error.message}
-      {isSuccess &&
-        data &&
-        data.map((promoter) => <h2 key={promoter.id}>{promoter.slug}</h2>)}
+      <PromoterList />
     </div>
   );
 };
