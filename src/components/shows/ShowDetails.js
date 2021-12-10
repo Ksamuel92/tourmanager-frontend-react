e34d9dd9a9e68b3d8a6cc6cb5fed793d85b1de10
Kyle Samuel <ksamuel92@gmail.com>
2021-12-10 13:07:46 -0500

    add Confirmation Dialog for deleting a show

--- src/components/shows/ShowDetails.js
+++ src/components/shows/ShowDetails.js
@@ -4,6 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Card from "@mui/material/Card";
 import ShowFormModal from "./ShowFormModal";
+import ConfirmationDialog from "../../Layout/ConfirmationDialog";
 import { useState } from "react";
 import { useDeleteShowMutation } from "../../features/shows/show-slice";
 
@@ -11,12 +12,19 @@ const ShowDetails = (props) => {
   const { venue, date, loadin, guarantee, merch, green_room, wifi, id } =
     props.show;
   const [open, setOpen] = useState(false);
+  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
   const [deleteShow, { isSuccess, isError }] = useDeleteShowMutation();
 
   const handleDelete = async (e) => {
     const response = deleteShow(id);
     console.log(response);
   };
+  const handleDeleteDialog = (e) => {
+    setOpenDeleteDialog(true);
+  };
+  const handleCloseDeleteDialog = (e) => {
+    setOpenDeleteDialog(false);
+  }
 
   const handleClickOpen = (e) => {
     setOpen(true);
@@ -54,11 +62,18 @@ const ShowDetails = (props) => {
         <Button size="small" onClick={handleClickOpen}>
           Edit
         </Button>
-        <Button size="small" onClick={handleDelete}>
+        <Button size="small" onClick={handleDeleteDialog}>
           Delete
         </Button>
       </CardActions>
       <ShowFormModal show={props.show} open={open} handleClose={handleClose} />
+      <ConfirmationDialog
+        open={openDeleteDialog}
+        handleClose={handleCloseDeleteDialog}
+        resource="Show"
+        action="Delete"
+        actionHandler={handleDelete}
+      />
     </Card>
   );
 };
