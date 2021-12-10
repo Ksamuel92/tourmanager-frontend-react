61715e1b79a83c909760b3faac414b11f4757d45
Kyle Samuel <ksamuel92@gmail.com>
2021-12-10 18:27:39 -0500

    add validation, update promoter for EditShowForm

--- src/components/shows/EditShowForm.js
+++ src/components/shows/EditShowForm.js
@@ -15,6 +15,8 @@ import { useState } from "react";
 import { useEditShowMutation } from "../../features/shows/show-slice";
 import Grid from "@mui/material/Grid";
 import { useSelector } from "react-redux";
+import Alert from "@mui/material/Alert";
+import { Fragment } from "react";
 
 const EditShowForm = (props) => {
   const {
@@ -40,7 +42,12 @@ const EditShowForm = (props) => {
     green_room,
     wifi_network,
     wifi_password,
-    promoter,
+  });
+
+  const [editPromoterState, setEditPromoterState] = useState({
+    name: promoter.name,
+    email: promoter.email,
+    id: promoter.id,
   });
   const [timeAndDate, setTimeAndDate] = useState({
     date,
@@ -48,7 +55,14 @@ const EditShowForm = (props) => {
   });
   const user = useSelector((store) => store.authReducer.user);
 
-  const [editShow, object] = useEditShowMutation(); //TODO: Extract object with loading
+  const [
+    editShow,
+    {
+      isSucces: editShowSuccess,
+      isError: editShowHasError,
+      error: editShowError,
+    },
+  ] = useEditShowMutation(); //TODO: Extract object with loading
 
   const handleChange = (e) => {
     const { name, value, checked } = e.target;
@@ -64,6 +78,14 @@ const EditShowForm = (props) => {
         [name]: value,
       });
     }
+  };
+
+  const handlePromoterChange = (e) => {
+    const { name, value } = e.target;
+    setEditPromoterState({
+      ...editPromoterState,
+      [name]: value,
+    });
   };
 
   const handleMomentDate = (e) => {
@@ -88,115 +110,118 @@ const EditShowForm = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const payload = {
+    const showPayload = {
       ...editShowFormState,
       ...timeAndDate,
-      user_id: user.id, //TODO  set to current-user
-      promoter_id: promoter.id,
+      promoter: { ...editPromoterState },
+      user_id: user.id,
     };
-    try {
-      const response = await editShow(payload).unwrap();
-      // TODO handle response if necessary
-      props.handleClose();
-    } catch (err) {
-      console.log(err);
-    }
+    editShow(showPayload);
+    props.handleClose();
   };
-
+  console.log(editShowError);
+  console.log(editPromoterState);
   return (
-    <form onSubmit={handleSubmit}>
-      <Grid spacing={3}>
-        <TextField
-          id="venue"
-          name="venue"
-          label="Venue"
-          onChange={handleChange}
-          value={editShowFormState.venue}
-          required
-        />
-        <DatePicker
-          name="date"
-          className="date"
-          label="Date"
-          value={editShowFormState.date}
-          onChange={handleMomentDate}
-          renderInput={(params) => <TextField {...params} />}
-        />
-        <TimePicker
-          name="loadin"
-          className="loadin"
-          label="Load In"
-          value={editShowFormState.loadin}
-          onChange={handleMomentTime}
-          renderInput={(params) => <TextField {...params} />}
-        />
-        <FormControl>
-          <InputLabel htmlFor="guarantee">Guarantee</InputLabel>
-          <OutlinedInput
-            id="guarantee"
-            name="guarantee"
-            value={editShowFormState.guarantee || ""}
-            onChange={handleChange}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Guarantee"
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="merch">Merch</InputLabel>
-          <OutlinedInput
-            id="merch"
-            value={editShowFormState.merch || ""}
-            name="merch"
-            onChange={handleChange}
-            startAdornment={<InputAdornment position="start">$</InputAdornment>}
-            label="Merch"
-          />
-        </FormControl>
-        <TextField
-          id="wifi_network"
-          name="wifi_network"
-          label="Wifi Network"
-          value={editShowFormState.wifi_network || ""}
-          onChange={handleChange}
-          required
-        />
-        <TextField
-          id="wifi_password"
-          name="wifi_password"
-          label="Wifi Password"
-          value={editShowFormState.wifi_password || ""}
-          onChange={handleChange}
-          required
-        />
-        <FormControlLabel
-          name="green_room"
-          checked={editShowFormState.green_room || false}
-          onChange={handleChange}
-          control={<Checkbox />}
-          label="Green Room"
-          labelPlacement="start"
-        />
-        <FormGroup></FormGroup>
-        <FormGroup row>
+    <Fragment>
+      {editShowHasError && <Alert severity="error"></Alert>}
+      <form onSubmit={handleSubmit}>
+        <Grid spacing={3}>
           <TextField
-            id="promoter-name"
-            name="name"
-            label="Promoter Name"
+            id="venue"
+            name="venue"
+            label="Venue"
             onChange={handleChange}
-            value={promoter.name}
+            value={editShowFormState.venue}
+            required
+          />
+          <DatePicker
+            name="date"
+            className="date"
+            label="Date"
+            value={editShowFormState.date}
+            onChange={handleMomentDate}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <TimePicker
+            name="loadin"
+            className="loadin"
+            label="Load In"
+            value={editShowFormState.loadin}
+            onChange={handleMomentTime}
+            renderInput={(params) => <TextField {...params} />}
+          />
+          <FormControl>
+            <InputLabel htmlFor="guarantee">Guarantee</InputLabel>
+            <OutlinedInput
+              id="guarantee"
+              name="guarantee"
+              value={editShowFormState.guarantee || ""}
+              onChange={handleChange}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              label="Guarantee"
+            />
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor="merch">Merch</InputLabel>
+            <OutlinedInput
+              id="merch"
+              value={editShowFormState.merch || ""}
+              name="merch"
+              onChange={handleChange}
+              startAdornment={
+                <InputAdornment position="start">$</InputAdornment>
+              }
+              label="Merch"
+            />
+          </FormControl>
+          <TextField
+            id="wifi_network"
+            name="wifi_network"
+            label="Wifi Network"
+            value={editShowFormState.wifi_network || ""}
+            onChange={handleChange}
+            required
           />
           <TextField
-            id="promoter-email"
-            name="email"
-            label="Promoter Email"
+            id="wifi_password"
+            name="wifi_password"
+            label="Wifi Password"
+            value={editShowFormState.wifi_password || ""}
             onChange={handleChange}
-            value={promoter.email}
+            required
           />
-        </FormGroup>
+          <FormControlLabel
+            name="green_room"
+            checked={editShowFormState.green_room || false}
+            onChange={handleChange}
+            control={<Checkbox />}
+            label="Green Room"
+            labelPlacement="start"
+          />
+          <FormGroup></FormGroup>
+          <FormGroup row>
+            <TextField
+              id="promoter-name"
+              name="name"
+              label="Promoter Name"
+              onChange={handlePromoterChange}
+              value={editPromoterState.name}
+            />
+            <TextField
+              id="promoter-email"
+              name="email"
+              label="Promoter Email"
+              onChange={handlePromoterChange}
+              value={editPromoterState.email}
+            />
+          </FormGroup>
 
-        <Button type="submit">Submit </Button>
-      </Grid>
-    </form>
+          <Button type="submit">Submit </Button>
+        </Grid>
+      </form>
+    </Fragment>
   );
 };
 
