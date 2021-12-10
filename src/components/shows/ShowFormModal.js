a4978d1c01ab83f8d54c8837387a9878056766a3
Kyle Samuel <ksamuel92@gmail.com>
2021-12-10 13:08:16 -0500

    Edit the edit show form modal

--- src/components/shows/ShowFormModal.js
+++ src/components/shows/ShowFormModal.js
@@ -12,22 +12,15 @@ const ShowFormModal = (props) => {
   // debugger;
   return (
     <Dialog open={props.open} onClose={props.handleClose}>
-      <DialogTitle>Subscribe</DialogTitle>
+      <DialogTitle>Edit Show</DialogTitle>
       <DialogContent>
-        <DialogContentText>
-          To subscribe to this website, please enter your email address here. We
-          will send updates occasionally.
-        </DialogContentText>
+        <DialogContentText>Edit Your Show</DialogContentText>
         <EditShowForm
           show={props.show}
           open={props.open}
           handleClose={props.handleClose}
         />
       </DialogContent>
-      <DialogActions>
-        <Button onClick={props.handleClose}>Cancel</Button>
-        <Button onClick={props.handleClose}>Subscribe</Button>
-      </DialogActions>
     </Dialog>
   );
 };
