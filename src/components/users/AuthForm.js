5fb8a9a39e572ddeaae704888f4cd7ffdc59591d
Kyle Samuel <ksamuel92@gmail.com>
2021-12-10 16:29:55 -0500

    add error handling and validation to authform

--- src/components/users/AuthForm.js
+++ src/components/users/AuthForm.js
@@ -1,4 +1,6 @@
 import { TextField, Button } from "@material-ui/core";
+import { Fragment } from "react";
+import Grid from "@material-ui/core/Grid";
 import useInput from "../../hooks/useInput";
 import {
   useSignUpMutation,
@@ -6,16 +8,28 @@ import {
 } from "../../features/auth/authEndpoints";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import Alert from "@mui/material/Alert";
+import Fade from "@mui/material/Fade";
 import Switch from "@mui/material/Switch";
 import Stack from "@mui/material/Stack";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Typography from "@mui/material/Typography";
 
 const AuthForm = () => {
-  const [signUpUser] = useSignUpMutation(); //TODO handle user object
-  const [loginUser] = useLoginMutation();
+  const [
+    signUpUser,
+    {
+      isSuccess: signUpSuccess,
+      isError: signUpError,
+      error: signUpErrorMessage,
+    },
+  ] = useSignUpMutation();
+  const [
+    loginUser,
+    { isSuccess: loginSuccess, isError: loginError, error: loginErrorMessage },
+  ] = useLoginMutation();
   const navigate = useNavigate();
-  const [loginForm, setLoginForm] = useState(false);
+  const [signUp, setsignUp] = useState(false);
   const { value: name, bind: bindName, reset: resetName } = useInput("");
   const { value: email, bind: bindEmail, reset: resetEmail } = useInput("");
   const {
@@ -25,71 +39,108 @@ const AuthForm = () => {
   } = useInput("");
 
   const handleSwitch = () => {
-    setLoginForm(!loginForm);
+    setsignUp((prevState) => !prevState);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!loginForm) {
-      try {
-        const signUpFormState = { user: { name, email, password } };
-        const response = await signUpUser(signUpFormState).unwrap();
-        if (response.status.code === 200) {
-          navigate("/shows");
-        }
-      } catch (err) {
-        console.log(err);
+    if (signUp) {
+      const signUpFormState = { user: { name, email, password } };
+      signUpUser(signUpFormState);
+      if (signUpSuccess) {
+        navigate("/shows");
       }
-    } else if (loginForm) {
-      try {
-        const loginFormState = { user: { email, password } };
-        const response = await loginUser(loginFormState).unwrap();
-        if (response.status.code === 200) {
-          navigate("/shows");
-        }
-      } catch (err) {
-        console.log(err);
+    } else if (!signUp) {
+      const loginState = { user: { email, password } };
+      loginUser(loginState);
+      if (loginSuccess) {
+        navigate("/shows");
       }
     }
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        {loginForm ? null : (
-          <TextField
-            id="name-input"
-            name="name"
-            label="Name"
-            type="text"
-            {...bindName}
-          />
-        )}
-        <TextField
-          id="email-input"
-          name="email"
-          label="Email"
-          type="email"
-          {...bindEmail}
-        />
-        <TextField
-          id="password-input"
-          name="password"
-          label="Password"
-          type="password"
-          {...bindPassword}
-        />
-        <Button type="submit" variant="contained">
-          Submit
-        </Button>
+    <Fragment>
+      {signUpError && (
+        <Alert severity="error">{signUpErrorMessage.data.status.message}</Alert>
+      )}
+      {loginError && (
+        <Alert severity="error">{loginErrorMessage.data.error}</Alert>
+      )}
+
+      <form onSubmit={handleSubmit} sx={{ outline: "white" }}>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          wrap="nowrap"
+          alignItems="center"
+          justifyContent="center"
+          style={{ minHeight: "50vh" }}
+        >
+          <Grid item>
+            <Typography variant="h6" align="center">
+              {signUp && "Sign Up"}
+              {!signUp && "Log In"}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Fade in={signUp}>
+              <TextField
+                id="name-input"
+                name="name"
+                label="Name"
+                type="text"
+                margin="dense"
+                {...bindName}
+                sx={{ marginBottom: "15px" }}
+              />
+            </Fade>
+            <Grid item>
+              <TextField
+                id="email-input"
+                name="email"
+                label="Email"
+                type="email"
+                margin="dense"
+                {...bindEmail}
+                sx={{ marginBottom: "15px" }}
+              />
+            </Grid>
+            <Grid item>
+              <TextField
+                id="password-input"
+                name="password"
+                label="Password"
+                type="password"
+                margin="dense"
+                {...bindPassword}
+                sx={{ marginBottom: "15px" }}
+              />
+            </Grid>
+            <Stack direction="row" spacing={1}>
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                align="center"
+              >
+                Submit
+              </Button>
+              {/* <Typography>Login</Typography> */}
+              <FormControlLabel
+                mt="15px"
+                control={<Switch color="primary" onChange={handleSwitch} />}
+                label={signUp ? "Log In" : "Sign Up"}
+                labelPlacement="bottom"
+              />
+              {/* <Typography>Sign Up</Typography> */}
+            </Stack>
+          </Grid>
+        </Grid>
       </form>
-      <Stack direction="row" spacing={1} alignItems="center">
-        <Typography>Sign Up</Typography>
-        <Switch onChange={handleSwitch} />
-        <Typography>Login</Typography>
-      </Stack>
-    </div>
+    </Fragment>
   );
 };
 
