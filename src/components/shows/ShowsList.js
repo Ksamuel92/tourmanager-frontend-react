78c6dc9b8718718ea84081eba99dbdd60354bee9
Kyle Samuel <ksamuel92@gmail.com>
2021-12-10 18:28:39 -0500

    make Show cards into a row

--- src/components/shows/ShowsList.js
+++ src/components/shows/ShowsList.js
@@ -1,6 +1,9 @@
 import { useGetShowsQuery } from "../../features/shows/show-slice";
+import Grid from "@mui/material/Grid";
 import ShowDetails from "../shows/ShowDetails";
 import { useSelector } from "react-redux";
+import { Link } from "@material-ui/core";
+import { Fragment } from "react";
 
 const ShowsList = () => {
   const { id } = useSelector((store) => store.authReducer.user);
@@ -8,13 +11,19 @@ const ShowsList = () => {
     refetchOnMountOrArgChange: true,
   });
   return (
-    <div>
-      {isLoading && "Loading..."}
-      {isError && error.message}
-      {isSuccess &&
-        data &&
-        data.map((show) => <ShowDetails key={show.id} show={show} />)}
-    </div>
+    <Fragment>
+      <Grid container row="true">
+        {isLoading && "Loading..."}
+        {isError && error.message}
+        {isSuccess &&
+          data &&
+          data.map((show, index) => (
+            <Grid item key={index}>
+              <ShowDetails key={show.id} show={show} />
+            </Grid>
+          ))}
+      </Grid>
+    </Fragment>
   );
 };
 
