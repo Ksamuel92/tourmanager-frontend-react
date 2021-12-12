9eb4935a8f3d177dc04c3001f5fbee36cd9fee49
Kyle Samuel <ksamuel92@gmail.com>
2021-12-12 13:25:09 -0500

    added automatic refetching in useGetPromotersQuery

--- src/components/promoters/PromoterList.js
+++ src/components/promoters/PromoterList.js
@@ -1,19 +1,35 @@
 import { useGetPromotersQuery } from "../../features/promoters/promoter-slice";
 import PromoterDetails from "./PromoterDetails";
+import { Fragment } from "react";
+import { useSelector } from "react-redux";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 const PromoterList = () => {
-  const { data, error, isLoading, isSuccess, isError } = useGetPromotersQuery();
+  const { id } = useSelector((store) => store.authReducer.user);
+  const { data, error, isLoading, isSuccess, isError } = useGetPromotersQuery(
+    id,
+    {
+      refetchOnMountOrArgChange: true,
+    }
+  );
   return (
-    <div>
-      This is the Promoters page
-      {isLoading && "Loading..."}
-      {isError && error.message}
-      {isSuccess &&
-        data &&
-        data.map((promoter) => (
-          <PromoterDetails key={promoter.id} promoter={promoter} />
-        ))}
-    </div>
+    <Fragment>
+      <Typography variant="h3" color="white" align="center">
+        Promoter List
+      </Typography>
+      <Grid container row="true" spacing={1} mt="15px">
+        {isLoading && "Loading..."}
+        {isError && error.message}
+        {isSuccess &&
+          data &&
+          data.map((promoter, index) => (
+            <Grid item key={index} md={3}>
+              <PromoterDetails key={promoter.id} promoter={promoter} />
+            </Grid>
+          ))}
+      </Grid>
+    </Fragment>
   );
 };
 
