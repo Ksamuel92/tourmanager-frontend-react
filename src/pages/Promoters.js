cb661aae9589402b3166e0176de375502367babf
Kyle Samuel <ksamuel92@gmail.com>
2021-12-13 12:30:57 -0500

    Remove "This is the promoters page"

--- src/pages/Promoters.js
+++ src/pages/Promoters.js
@@ -3,7 +3,6 @@ import PromoterList from "../components/promoters/PromoterList";
 const Promoters = () => {
   return (
     <div>
-      This is the Promoters page
       <PromoterList />
     </div>
   );
