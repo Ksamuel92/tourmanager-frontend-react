9105bb660d3c9c7a639c4dd6de42f43103dc59e9
Kyle Samuel <ksamuel92@gmail.com>
2021-12-31 12:10:41 -0600

    change baseURL for heroku deployment

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const tourManagerApi = createApi({
   reducerPath: "tourManagerApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3001/",
+    baseUrl: "https://tourmanager-backend.herokuapp.com/",
     refetchOnFocus: true,
     prepareHeaders: (headers, { getState }) => {
       const token = getState().authReducer.token;
