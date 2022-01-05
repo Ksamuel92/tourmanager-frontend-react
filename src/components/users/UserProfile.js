4e5daba8995e3ad293e87ee013b9ba74818e0615
Kyle Samuel <ksamuel92@gmail.com>
2022-01-05 13:57:38 -0600

    consolidate imports in UserProfile

--- src/components/users/UserProfile.js
+++ src/components/users/UserProfile.js
@@ -1,15 +1,17 @@
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import EmailIcon from "@mui/icons-material/Email";
-import { Box } from "@material-ui/core";
-import CardContent from "@mui/material/CardContent";
-import Card from "@mui/material/Card";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import {
+  Box,
+  CardContent,
+  Card,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
 
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 const UserProfile = () => {
   const { user } = useSelector((store) => store.authReducer);
 
